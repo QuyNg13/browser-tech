1c482e1b2ab5dbb300b9d7675549195ad2d85591
QuyNg13 <quythieu34@gmail.com>
2025-03-05 15:17:07 +0100

    opschonen

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ document.querySelectorAll('input[name="partner?"]').forEach(function(radio) {
         const fieldsets = document.querySelectorAll('form:nth-of-type(2) fieldset:nth-of-type(2),form:nth-of-type(2) fieldset:nth-of-type(3)');
         fieldsets.forEach(function(fieldset) {
             if (document.querySelector('input[name="partner?"]:checked').value === 'nee') {
-                fieldset.hidden = true;
+                // fieldset.hidden = true;
                 fieldset.disabled = true;
             } else {
-                fieldset.hidden = false;
+                // fieldset.hidden = false;
                 fieldset.disabled = false;
             }
         });

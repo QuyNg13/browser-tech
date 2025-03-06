b3acb84f6327b01c4b556d9c39d7923fa6a69bac
QuyNg13 <quythieu34@gmail.com>
2025-03-06 13:31:21 +0100

    display toggle in js en begin buttons

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-document.querySelectorAll('input[name="partner?"]').forEach(function(radio) {
-    radio.addEventListener('change', function() {
+document.querySelectorAll('.checkboxes fieldset:first-of-type input').forEach(function (radio) {
+    radio.addEventListener('change', function () {
         const fieldsets = document.querySelectorAll('.checkboxes fieldset:nth-of-type(2),.checkboxes fieldset:nth-of-type(3)');
-        fieldsets.forEach(function(fieldset) {
-            const nee = (document.querySelector('.checkboxes fieldset:first-of-type input:checked').value === 'nee');
+        const nee = (document.querySelector('.checkboxes fieldset:first-of-type input:checked').value === 'nee');
+        fieldsets.forEach(function (fieldset) {
+            if (nee) {
+                fieldset.style.display = 'none';
+            } else {
+                fieldset.style.display = 'flex';
+                fieldset.style.flexDirection = 'column';
+            }
             fieldset.disabled = nee;
             fieldset.querySelectorAll('input').forEach(function (input) {
                 if (nee) {

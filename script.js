3c8f6fb6cae3c535a1076c88982a8a1f67f7d0fa
QuyNg13 <quythieu34@gmail.com>
2025-03-06 11:53:18 +0100

    dynamic required attribute

--- script.js
+++ script.js
@@ -2,11 +2,15 @@ document.querySelectorAll('input[name="partner?"]').forEach(function(radio) {
     radio.addEventListener('change', function() {
         const fieldsets = document.querySelectorAll('.checkboxes fieldset:nth-of-type(2),.checkboxes fieldset:nth-of-type(3)');
         fieldsets.forEach(function(fieldset) {
-            if (document.querySelector('input[name="partner?"]:checked').value === 'nee') {
-                fieldset.disabled = true;
-            } else {
-                fieldset.disabled = false;
-            }
+            const nee = (document.querySelector('.checkboxes fieldset:first-of-type input:checked').value === 'nee');
+            fieldset.disabled = nee;
+            fieldset.querySelectorAll('input').forEach(function (input) {
+                if (nee) {
+                    input.removeAttribute('required');
+                } else {
+                    input.setAttribute('required', 'required');
+                }
+            });
         });
     });
 });

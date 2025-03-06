d769fa935c049d4a27609cab15783c75ad39ac62
QuyNg13 <quythieu34@gmail.com>
2025-03-06 11:33:48 +0100

    sepperate pages & universal question skip

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.querySelectorAll('input[name="partner?"]').forEach(function(radio) {
     radio.addEventListener('change', function() {
-        const fieldsets = document.querySelectorAll('form:nth-of-type(2) fieldset:nth-of-type(2),form:nth-of-type(2) fieldset:nth-of-type(3)');
+        const fieldsets = document.querySelectorAll('.checkboxes fieldset:nth-of-type(2),.checkboxes fieldset:nth-of-type(3)');
         fieldsets.forEach(function(fieldset) {
             if (document.querySelector('input[name="partner?"]:checked').value === 'nee') {
                 fieldset.disabled = true;

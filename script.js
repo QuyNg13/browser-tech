2cbed4ab47b49a3d739c35d291925b9c6922fc02
QuyNg13 <quythieu34@gmail.com>
2025-03-06 15:29:11 +0100

    display toggle fix & buttons

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 document.querySelectorAll('.checkboxes fieldset:first-of-type input').forEach(function (radio) {
     radio.addEventListener('change', function () {
         const fieldsets = document.querySelectorAll('.checkboxes fieldset:nth-of-type(2),.checkboxes fieldset:nth-of-type(3)');
-        const nee = (document.querySelector('.checkboxes fieldset:first-of-type input:checked').value === 'nee');
+        const ja = (document.querySelector('.checkboxes fieldset:first-of-type input:checked').value === 'ja');
         fieldsets.forEach(function (fieldset) {
-            if (nee) {
-                fieldset.style.display = 'none';
-            } else {
+            if (ja) {
                 fieldset.style.display = 'flex';
-                fieldset.style.flexDirection = 'column';
+                fieldset.style.flexDirection = 'column';  
+            } else {
+                fieldset.style.display = 'none';
             }
-            fieldset.disabled = nee;
+            fieldset.disabled = !ja;
             fieldset.querySelectorAll('input').forEach(function (input) {
-                if (nee) {
-                    input.removeAttribute('required');
-                } else {
+                if (ja) {
                     input.setAttribute('required', 'required');
+                } else {
+                    input.removeAttribute('required');
                 }
             });
         });
